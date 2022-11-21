bce39f3b2312f958d0eabc89c3f8900ba207d663
EstebanVU <estebanvargasu@gmail.com>
2022-11-20 19:49:19 -0600

    Save image

--- lib/views/screens/create_post.dart
+++ lib/views/screens/create_post.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:mongo_dart/mongo_dart.dart' as m;
-//import 'package:utility_plus/src/database/note_db.dart';
-//import 'package:utility_plus/src/models/note.dart';
 import '../../models/post.dart';
 import '../../services/adoptionPostService.dart';
 import '../../services/lostPostService.dart';
@@ -15,7 +13,6 @@ import '../../util/const.dart';
 import '../../util/view_image_handler.dart';
 
 class CreatePost extends StatefulWidget {
-  
   final int postType;
 
   const CreatePost({Key key, this.postType}) : super(key: key);
@@ -26,11 +23,11 @@ class CreatePost extends StatefulWidget {
 
 class _CreatePostState extends State<CreatePost> {
   /// Variables
-  File _imageFile;
+  List<File> _postImages = [];
   final _titleField = TextEditingController();
   final _contentField = TextEditingController();
   bool _loadindicador = false;
-  
+
   @override
   void initState() {
     super.initState();
@@ -40,174 +37,183 @@ class _CreatePostState extends State<CreatePost> {
   void dispose() {
     super.dispose();
   }
-   
+
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async => !_loadindicador,
-      child: IgnorePointer(
-        ignoring: _loadindicador,
-        child: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
-          child: Scaffold(
-            resizeToAvoidBottomInset: false,
-            appBar: AppBar(
-              iconTheme: IconThemeData(
-                color: Theme.of(context).textTheme.headline6.color,
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Theme.of(context).textTheme.headline6.color,
+        ),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Container(
+            margin: EdgeInsets.only(top: 15, bottom: 10),
+            child: MediaQuery.of(context).platformBrightness == Brightness.dark
+                ? Image.asset(
+                    '${Constants.logoWhite}',
+                    height: 120,
+                  )
+                : Image.asset(
+                    '${Constants.logoBlack}',
+                    height: 120,
+                  )),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.check),
+            onPressed: () {
+              //Publicar post
+              insertPost();
+            },
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _loadindicador ? LinearProgressIndicator() : Container(height: 4),
+              TextField(
+                controller: _titleField,
+                decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).textTheme.headline6.color,
+                        width: 1.0,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).textTheme.headline6.color,
+                      ),
+                    ),
+                    border: InputBorder.none,
+                    hintText: 'Título',
+                    hintStyle: TextStyle(
+                        color: Theme.of(context).textTheme.headline6.color)),
+                style: TextStyle(
+                    color: Theme.of(context).textTheme.headline6.color,
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold),
               ),
-              backgroundColor: Theme.of(context).primaryColor,
-              title: Container(
-                  margin: EdgeInsets.only(top: 15, bottom: 10),
-                  child: MediaQuery.of(context).platformBrightness ==
-                          Brightness.dark
-                      ? Image.asset(
-                          '${Constants.logoWhite}',
-                          height: 120,
-                        )
-                      : Image.asset(
-                          '${Constants.logoBlack}',
-                          height: 120,
-                        )),
-              centerTitle: true,
-              actions: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.check),
-                  onPressed: () {
-                    //Publicar post
-                    insertPost();
-                  },
-                )
-              ],
-            ),
-            body: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  _loadindicador
-                      ? LinearProgressIndicator()
-                      : Container(height: 4),
-                  TextField(
-                    controller: _titleField,
+              SizedBox(
+                height: 150,
+                child: TextField(
+                    controller: _contentField,
                     decoration: InputDecoration(
                         enabledBorder: UnderlineInputBorder(
                           borderSide: BorderSide(
-                            color: Theme.of(context).textTheme.headline6.color,
+                            color: Colors.transparent,
                             width: 1.0,
                           ),
                         ),
                         focusedBorder: UnderlineInputBorder(
                           borderSide: BorderSide(
-                            color: Theme.of(context).textTheme.headline6.color,
+                            color: Colors.transparent,
                           ),
                         ),
                         border: InputBorder.none,
-                        hintText: 'Título',
+                        hintText: 'Contenido',
                         hintStyle: TextStyle(
                             color:
                                 Theme.of(context).textTheme.headline6.color)),
                     style: TextStyle(
-                        color: Theme.of(context).textTheme.headline6.color,
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Expanded(
-                    child: TextField(
-                        controller: _contentField,
-                        decoration: InputDecoration(
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.transparent,
-                                width: 1.0,
-                              ),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.transparent,
-                              ),
-                            ),
-                            border: InputBorder.none,
-                            hintText: 'Contenido',
-                            hintStyle: TextStyle(
-                                color: Theme.of(context)
-                                    .textTheme
-                                    .headline6
-                                    .color)),
-                        style: TextStyle(
-                          color: Theme.of(context).textTheme.headline6.color,
-                        ),
-                        expands: true,
-                        minLines: null,
-                        maxLines: null,
-                        keyboardType: TextInputType.multiline),
-                  ),
-                  Divider(
-                    thickness: 1,
-                  ),
-                  Container(
-                      child: _imageFile == null
-                          ? Container()
-                          : Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                ConstrainedBox(
-                                    constraints: const BoxConstraints(
-                                        maxHeight: 200, maxWidth: 330),
-                                    child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(15),
-                                        child: ImageFullScreenWrapperWidget(
-                                          dark: true,
-                                          child: Image.file(_imageFile),
-                                        ))),
-                                Container(
-                                  width: 40,
-                                  
-                                  decoration: BoxDecoration(
-                                      color: Theme.of(context).textTheme.headline6.color,
-                                      borderRadius: const BorderRadius.only(
-                                          bottomRight: Radius.circular(15),
-                                          topRight: Radius.circular(15))),
-                                  child: IconButton(
-                                    icon: Icon(
-                                      Icons.delete,
-                                      color: Theme.of(context).primaryColor,
-                                    ),
-                                    onPressed: () {
-                                      // do something
-                                      _imageFile = null;
-                                      setState(() {});
-                                    },
-                                  ),
-                                ),
-                              ],
-                            )),
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: Icon(
-                          Icons.photo_size_select_actual_outlined,
-                          color: Theme.of(context).textTheme.headline6.color,
-                        ),
-                        onPressed: () {
-                          // do something
-                          _getFromGallery();
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(
-                          Icons.camera_alt_outlined,
-                          color: Theme.of(context).textTheme.headline6.color,
-                        ),
-                        onPressed: () {
-                          // do something
-                          _getFromCamera();
-                        },
-                      ),
-                    ],
-                  ),
-                ],
+                      color: Theme.of(context).textTheme.headline6.color,
+                    ),
+                    minLines: null,
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline),
               ),
-            ),
+              Divider(
+                thickness: 1,
+              ),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                primary: false,
+                padding: EdgeInsets.all(5),
+                itemCount: _postImages.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 200 / 200,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Stack(fit: StackFit.expand, children: <Widget>[
+                      ImageFullScreenWrapperWidget(
+                        dark: true,
+                        child: Image.file(
+                          _postImages[index],
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Positioned(
+                        right: -6,
+                        top: -6,
+                        child: CircleAvatar(
+                          radius: 18,
+                          backgroundColor: Colors.black.withOpacity(0.5),
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.delete,
+                              size: 18,
+                              color:
+                                  Theme.of(context).textTheme.headline6.color,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _postImages.removeAt(index);
+                              });
+                            },
+                          ),
+                        ),
+                      )
+                    ]),
+                  );
+                },
+              ),
+            ],
           ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Divider(
+              thickness: 1,
+            ),
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.photo_size_select_actual_outlined,
+                    color: Theme.of(context).textTheme.headline6.color,
+                  ),
+                  onPressed: () {
+                    // do something
+                    _getFromGallery();
+                  },
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.camera_alt_outlined,
+                    color: Theme.of(context).textTheme.headline6.color,
+                  ),
+                  onPressed: () {
+                    // do something
+                    _getFromCamera();
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
@@ -217,12 +223,11 @@ class _CreatePostState extends State<CreatePost> {
   _getFromGallery() async {
     XFile pickedFile = await ImagePicker().pickImage(
       source: ImageSource.gallery,
-      //maxWidth: 200,
-      //maxHeight: 200,
+      imageQuality: 25,
     );
     if (pickedFile != null) {
       setState(() {
-        _imageFile = File(pickedFile.path);
+        _postImages.add(File(pickedFile.path));
       });
     }
   }
@@ -231,27 +236,30 @@ class _CreatePostState extends State<CreatePost> {
   _getFromCamera() async {
     XFile pickedFile = await ImagePicker().pickImage(
       source: ImageSource.camera,
-      // maxWidth: 200,
-      // maxHeight: 200,
+      imageQuality: 25,
     );
     if (pickedFile != null) {
       setState(() {
-        _imageFile = File(pickedFile.path);
+        _postImages.add(File(pickedFile.path));
       });
     }
   }
 
-  Future<String> _uploadToFirebase(String pathName) async {
-    if (_imageFile != null) {
+  Future<List<String>> _uploadToFirebase(String pathName) async {
+    if (_postImages.isNotEmpty) {
       try {
         if (await hasNetwork()) {
           // Upload file and metadata to the path 'images/mountains.jpg'
           final uploadTask =
               FirebaseStorage.instance.ref().child('images/$pathName');
+          List<String> data = [];
+          for (var element in _postImages) {
+            await uploadTask.putFile(element);
+            String url = await uploadTask.getDownloadURL();
+            data.add(url);
+          }
 
-          await uploadTask.putFile(_imageFile);
-          String url = await uploadTask.getDownloadURL();
-          return url;
+          return data;
         } else {
           throw ('Internet error');
         }
@@ -266,12 +274,13 @@ class _CreatePostState extends State<CreatePost> {
     ScaffoldMessenger.of(context).removeCurrentSnackBar();
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(value)));
   }
-  
+
   insertPost() async {
     if (_titleField.text.isEmpty ||
-        _contentField.text.isEmpty) {
-          showInSnackBar('Es necesario establecer el titulo y el contenido para la publicación.');
-      
+        _contentField.text.isEmpty ||
+        _postImages.isEmpty) {
+      showInSnackBar(
+          'Es necesario establecer un título, contenido y al menos una imagen para hacer la publicación.');
     } else {
       try {
         _loadindicador = true;
@@ -280,7 +289,7 @@ class _CreatePostState extends State<CreatePost> {
 
         final post = Post(
             id: id,
-            image: '', //await _uploadToFirebase(id.$oid)
+            image: await _uploadToFirebase(id.$oid),
             title: _titleField.text,
             description: _contentField.text,
             publishedDate: DateTime.now(),
